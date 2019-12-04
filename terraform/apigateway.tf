8e90849146ceca0a17a031559648633d965ef571
Sam Watson <samuelpwatson@gmail.com>
2019-12-03 16:28:51 -0800

    get rid of unnecessary integration response

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -25,7 +25,7 @@ resource "aws_api_gateway_integration" "very_nice_lambda" {
   rest_api_id             = aws_api_gateway_rest_api.samsverynice.id
   resource_id             = aws_api_gateway_resource.base.id
   http_method             = aws_api_gateway_method.base_get.http_method
-  type                    = "AWS"
+  type                    = "AWS_PROXY"
   integration_http_method = "POST"
   uri                     = aws_lambda_function.very_nice.invoke_arn
 }
@@ -37,16 +37,6 @@ resource "aws_api_gateway_method_response" "base_get_200" {
   status_code = "200"
   response_models = {
     "text/plain" = "Empty"
-  }
-}
-
-resource "aws_api_gateway_integration_response" "very_nice_lambda_200" {
-  rest_api_id = aws_api_gateway_rest_api.samsverynice.id
-  resource_id = aws_api_gateway_resource.base.id
-  http_method = aws_api_gateway_method.base_get.http_method
-  status_code = aws_api_gateway_method_response.base_get_200.status_code
-  response_templates = {
-    "application/json" = ""
   }
 }
 
