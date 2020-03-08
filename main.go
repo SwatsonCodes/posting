3c48eda31ae281a01c188872ac813ebd3caf5bae
Sam Watson <samuelpwatson@gmail.com>
2020-03-07 17:58:31 -0800

    add ability to run server locally

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
+	"github.com/gorilla/mux"
 )
 
 const cowsay string = `
- __________
-< GO AWAY. >
- ----------
+ _________
+< GO AWAY >
+ ---------
         \   ^__^
          \  (oo)\_______
             (__)\       )\/\
@@ -18,16 +23,27 @@ const cowsay string = `
                 ||     ||
 `
 
+var adapter *gorillamux.GorillaMuxAdapter
+var router *mux.Router
+
+func init() {
+	log.Print("cold start")
+	router = mux.NewRouter()
+	router.HandleFunc("/", goAway)
+	adapter = gorillamux.New(router)
+}
+
+func goAway(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, cowsay)
+}
+
 func main() {
-	lambda.Start(func() (events.APIGatewayProxyResponse, error) {
-		return events.APIGatewayProxyResponse{
-				StatusCode: http.StatusOK,
-				Body:       cowsay,
-				Headers: map[string]string{
-					"Content-Type":  "text/plain",
-					"Cache-Control": "no-cache",
-				},
-			},
-			nil
-	})
+	if _, isInsideLambda := os.LookupEnv("LAMBDA_TASK_ROOT"); isInsideLambda {
+		lambda.Start(func(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+			return adapter.Proxy(req)
+		})
+	} else {
+		log.Print("serving on port 8000")
+		http.ListenAndServe(":8000", router)
+	}
 }
